25befeb940ab4d75b9ce10045fc11d1b68b8e8d4
ODo56 <okhalil@dosolution.dev>
2024-12-12 06:49:50 +0000

    added some fixes and added signout component

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Sidebar from "@/components/Sidebar";
 import Lock from "@/components/lock";
+import SignOut from "@/components/signout";
 export default function Users() {
 
   const ui = (<div className="flex h-screen bg-gray-900 text-white">
@@ -9,6 +10,7 @@ export default function Users() {
     <main className="flex-1 p-6">
       <h1 className="text-2xl font-semibold mb-6">Users Page</h1>
       <p>This is the Users page!</p>
+      <SignOut/>
     </main>
   </div>)
   return (
